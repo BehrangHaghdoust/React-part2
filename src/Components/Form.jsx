d1093609f8ede56b86c6bbbde319b784f00e6338
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2022-03-14 22:35:16 +0330

    checkbox & radio btn

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,23 +1,42 @@
 const Form = () => {
 
-    const handleChange = (value) => {
-        console.log(value);
+    // const handleChange = (value) => {
+    //     console.log(value);
+    // }
+
+    // const changeState = (e) => {
+    //     console.log(e.target.value);
+
+    // }
+    // const changeState2 = (e) => {
+    //     // console.log(e.target.selectedOptions);
+    //     let selectedOptions = [...e.target.selectedOptions ].map(option => option.value);
+    //     console.log(selectedOptions);
+    // }
+
+    let checkItems = [];
+
+    const changeBox = (e) => {
+
+
+        let item = checkItems.find(item => item === e.target.value);
+
+        if (item) {
+            checkItems = checkItems.filter(item => item !== e.target.value)
+        } else {
+            checkItems.push(e.target.value)
+        }
+
+        console.log(checkItems);
     }
 
-    const changeState = (e) => {
+    const radioChangeBox=(e)=>{
         console.log(e.target.value);
-
-    }
-    const changeState2 = (e) => {
-        // console.log(e.target.selectedOptions);
-        let selectedOptions = [...e.target.selectedOptions ].map(option => option.value);
-        console.log(selectedOptions);
-
     }
 
     return (
         <div>
-            <input onChange={(e) => handleChange(e.target.value)} type="text" />
+            {/* <input onChange={(e) => handleChange(e.target.value)} type="text" />
             <br />
             <textarea onChange={(e) => handleChange(e.target.value)} style={{ marginTop: '20px' }}></textarea>
             <br />
@@ -36,7 +55,18 @@ const Form = () => {
                 <option value="CSS">CSS</option>
                 <option value="JAVASCRIPT">HTML</option>
                 <option value="PHP">PHP</option>
-            </select>
+            </select> */}
+            <input type="checkbox" onChange={(e) => changeBox(e)} value="check1" />check1
+            <br />
+            <input type="checkbox" onChange={(e) => changeBox(e)} value="check2" />check2
+            <br />
+            <input type="checkbox" onChange={(e) => changeBox(e)} value="check3" />check3
+            <br />
+
+            <input type="radio" name="gender" onChange={(e) => radioChangeBox(e)} value="Male" />Male
+            <br />
+            <input type="radio" name="gender" onChange={(e) => radioChangeBox(e)} value="Female" />Female
+            <br />
 
         </div>
     )
