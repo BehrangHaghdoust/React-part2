85555b2c0d7e92b742cb8b494723eef3646a39d9
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2022-03-18 11:41:13 +0330

    get prevState

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -10,13 +10,18 @@ import {useState} from 'react';
 function App() {
   const [count , setCount] = useState(0)
 
-  const AddCount = ()=>{
-    setCount (count + 1)
+const fiveIncrease = ()=>{
+  for(let i = 0 ; i<5 ; i++){
+    setCount((prevState)=> prevState + 1)
   }
+}
   return (
     <div className="App">
       <h1>start part 2</h1>
-      <button onClick={()=>AddCount()}>Count : {count}</button>
+      <button onClick={()=> setCount (count + 1)}>Increase</button>
+      <button onClick={fiveIncrease}>Increase 5 Number</button>
+      <h1>{count}</h1>
+      <button onClick={()=> setCount (count - 1)}>Decrease</button>
     </div>
   );
 }
