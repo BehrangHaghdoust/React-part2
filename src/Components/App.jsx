0a799b0cb39146e0f416c1de3a8ce9c17aa85259
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2022-03-19 22:32:38 +0330

    fetch by useEffect -- part 2 use loading

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -11,19 +11,35 @@ function App() {
 
 
   const [posts , setPost] = useState(null);
+  const [loading , setLoading] =useState(true);
+  const [error , setError] =useState();
+
 
 
 useEffect(()=>{
  fetch('https://jsonplaceholder.typicode.com/posts')
 .then(res => {return res.json()})
-.then(data => setPost(data))
+.then(data => {
+  setPost(data)
+  setLoading(false);
+  setError('')
+
+})
+.catch(err => {
+  setError(err.message)
+setLoading(false)
+}
+)
 })
 
   return (
     <div className="App">
-      <h1>fetch by useEffect</h1>
+      <h1>fetch by useEffect -- part 2 use loading</h1>
 
-// instade of "posts == null ? '' " we can use &&
+      {loading && <h2>Loading ...</h2> }
+      {error && <h2>{error}</h2>}
+
+{ /* instade of "posts == null ? '' " we can use && */}
 {posts == null ? '' : posts.map(post => (
 <div key={post.id}>
   <h3>{post.title}</h3>
