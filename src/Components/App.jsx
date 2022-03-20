eabbb2eb812d89b931dc6d49da8bb6aaee2e9a2e
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2022-03-20 08:34:27 +0330

    start using useReducer

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,25 +1,48 @@
+// import {useState} from 'react';
+// function App() {
+// const [count , setCount] = useState(0)
+//   return (
+//     <div className="App">
+//       <h1>useReducer</h1>
 
-import '../style/App.css';
-import React from 'react';
-import CompoA  from './CompoA';
+//       <h3>{count}</h3>
+//       <button onClick={()=> setCount(count + 1)}>Increment</button>
+//       <button onClick={()=>setCount(count - 1)}>Decrement</button>
+//       <button onClick={()=> setCount(0)}>Reset</button>
+//     </div>
+//   );
+// }
+// export default App;
 
 
 
+import { useReducer } from 'react';
 
-{/* here we want to send data from App to Compo B */}
-
-export const userContext = React.createContext();
-
-function App() {
-  return (
-    <userContext.Provider value={'Behrang Haghdoust'}>
-
-    <div className="App">
-      <h1>useContext</h1>
-      <CompoA/>
-    </div>
-    </userContext.Provider>
-  );
+const intialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return intialState
+    default:
+      return state
+  }
 }
 
+function App() {
+  const [count, dispatch] = useReducer(reducer, intialState);
+  return (
+    <div className="App">
+      <h1>useReducer</h1>
+      <h3>{count}</h3>
+      <button onClick={()=>dispatch('increment')}>Increment</button>
+      <button onClick={()=>dispatch('decrement')}>Decrement</button>
+      <button onClick={()=>dispatch('reset')}>Reset</button>
+    </div>
+  );
+}
 export default App;
+
