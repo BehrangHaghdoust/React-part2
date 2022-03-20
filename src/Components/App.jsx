5bffbbb1f829c0c0e50523056db8e96ee48ff4a9
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2022-03-20 10:16:15 +0330

    using servral useReducer

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -18,35 +18,36 @@
 
 import { useReducer } from 'react';
 
-const intialState = {
-  count: 0
-};
+const intialState = 0;
 const reducer = (state, action) => {
-  switch (action.type) {
+  switch (action) {
     case 'increment':
-      return {...state ,count : state.count + action.payload}
-      case 'incrementFive':
-        return {...state ,count : state.count + action.payload}
+      return state + 1
     case 'decrement':
-      return {...state , count : state.count - action.payload}
+      return state - 1
     case 'reset':
       return intialState
     default:
-      return
+      return state
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, intialState);
+  const [count, dispatch] = useReducer(reducer, intialState);
+  const [count2, dispatch2] = useReducer(reducer, intialState);
   return (
     <div className="App">
-      <h1>useReducer --part 2 using object</h1>
-      <h3>{state.count}</h3>
-      <button onClick={() => dispatch({type:'increment', payload:1})}>Increment</button>
-      {/* <button onClick={() => dispatch('incrementFive')}>Increment 5</button> */}
-      <button onClick={() => dispatch({type:'incrementFive', payload:5})}>Increment 5</button>
-      <button onClick={() => dispatch({type:'decrement',payload:1})}>Decrement</button>
-      <button onClick={() => dispatch({type:'reset'})}>Reset</button>
+      <h1>using servral useReducer</h1>
+      <h3>{count}</h3>
+      <button onClick={()=>dispatch('increment')}>Increment</button>
+      <button onClick={()=>dispatch('decrement')}>Decrement</button>
+      <button onClick={()=>dispatch('reset')}>Reset</button>
+      <br />
+      <h3>{count2}</h3>
+      <button onClick={()=>dispatch2('increment')}>Increment</button>
+      <button onClick={()=>dispatch2('decrement')}>Decrement</button>
+      <button onClick={()=>dispatch2('reset')}>Reset</button>
+      <br />
     </div>
   );
 }
