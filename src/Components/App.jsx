a8bb16f046bdeceed4c4f1bcf0e7a6c312928137
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2022-03-21 19:19:45 +0330

    working with useMemo in order to catch data in a function

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,33 +1,12 @@
-
-import {useCallback,useState} from 'react';
-import Count from './Count'
-import Button from './Button'
-import Title from './Title'
-
-
-{/* we use useCallback in order to prevent rerender 2 buttens put in a stracture */}
-
+import Counter from '../Components/Counter'
 
 function App() {
-  const [countOne, setCountOne] = useState(0);
-  const [countTwo, setCountTwo] = useState(0);
 
-const incrementCountOne = useCallback(() => {
-  setCountOne(countOne + 1)
-},[countOne])
-const incrementCountTwo =useCallback(() => {
-  setCountTwo(countTwo + 1)
-
-}, [countTwo])
 
   return (
     <>
-   <Title/>
-   <Count text="count one" count={countOne} />
-   <Button handleClick={incrementCountOne}>Increment Count One</Button>
-
-   <Count text="count Two" count={countTwo} />
-   <Button handleClick={incrementCountTwo}>Increment Count Two</Button>
+<h1>working with useMemo</h1>
+<Counter/>
     </>
   );
 }
