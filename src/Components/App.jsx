24cd47eb4ed84a4a6905a9b28dd7d374de31277e
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2022-03-21 23:15:28 +0330

    making  custom hooks

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,14 @@
-import Login from '../Components/Login'
-
+import useCounter from "../Hooks/useCounter";
 function App() {
 
-
+const {count,increment,decrement,reset}= useCounter(100)
   return (
     <>
-<h1>working with useRef</h1>
-<Login/>
+<h1>working with custom hooks</h1>
+<h3>Count ={count}</h3>
+<button onClick={increment}>Increment</button>
+<button onClick={decrement}>Decrement</button>
+<button onClick={reset}>Reset</button>
     </>
   );
 }
